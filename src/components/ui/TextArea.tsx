0a5f34aa77c36b9c5132d30f353b87c653c98b58
DongSeong Seo <sds901234@naver.com>
2025-12-06 22:36:11 +0900

    feat: TextArea 컴포넌트 추가

---
+++ src/components/ui/TextArea.tsx
@@ -0,0 +1,30 @@
+interface TextAreaProps {
+  placeholder: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export default function TextArea({
+  value,
+  onChange,
+  placeholder,
+}: TextAreaProps) {
+  const classes = `
+    bg-[var(--grey-50)] w-[568px] h-[84px]
+    px-[16px] py-[12px]  
+    text-[var(--color-${value?.length === 0 ? "grey-300" : "grey-800"})] text-[16px]
+    font-[500] 
+    focus:outline-none
+    focus:border-none
+    resize-none
+  `;
+
+  return (
+    <textarea
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      className={classes}
+      placeholder={placeholder}
+    ></textarea>
+  );
+}
