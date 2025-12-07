1309ab8698daa34053449776b2f6f0d114fc9464
DongSeong Seo <sds901234@naver.com>
2025-12-07 14:45:29 +0900

    feat: Button 컴포넌트 수정

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,20 +5,24 @@ type ButtonVariant = "primary" | "secondary";
 interface ButtonProps {
   variant: ButtonVariant;
   children?: ReactNode;
+  disabled?: boolean;
+  onClick?: () => void;
 }
-const colorClasses = {
-  primary: "bg-[var(--color-primary)] hover:bg-[var(--color-primary-10b)]",
-  secondary:
-    "bg-[var(--color-primary-light)] hover:bg-[var(--color-primary-light-10b)]",
-} as const;
-export default function Button({ variant, children }: ButtonProps) {
-  const shape = `min-w-[88px] h-[48px] py-[12px] px-[16px] rounded-[5px]`;
-  const align = `flex justify-center items-center`;
-  const text = `text-white sub-title-s`;
-  const focus = `focus:border-[1.5px] focus:border-[var(--color-fuchsia)]`;
-  const etc = `cursor-pointer`;
 
-  const classes = `${colorClasses[variant]} ${shape} ${align} ${text} ${focus} ${etc}`;
-
-  return <div className={classes}>{children}</div>;
+export default function Button({
+  variant,
+  children,
+  disabled,
+  onClick,
+}: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className={`btn btn-${variant}`}
+      disabled={disabled !== undefined ? disabled : false}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
