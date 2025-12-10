e58f78152e461a99ccf5fe94e099c25c8f253d22
DongSeong Seo <sds901234@naver.com>
2025-12-10 21:36:49 +0900

    feat: 로그인 api 추가

--- src/api/response.type.ts
+++ src/api/response.type.ts
@@ -10,11 +10,22 @@ export interface CheckNicknameResponse {
   message: string;
 }
 
-export interface SignUpResponse {
+export interface CommonResponse {
   success: boolean;
-  message: string;
   error?: {
     message: string;
     statusCode: number;
   };
+}
+
+export interface SignUpResponse extends CommonResponse {
+  message: string;
+}
+
+export interface LoginResponse extends CommonResponse {
+  message: string;
+  accessToken: string;
+  refreshToken: string;
+  isFirstLogin: boolean;
+  isDuplicateLogin: boolean;
 }
