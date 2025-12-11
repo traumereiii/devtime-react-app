634b1d17c2ac98a791886e22a65989df5920a690
DongSeong Seo <sds901234@naver.com>
2025-12-11 14:37:05 +0900

    feat: file to base64 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,16 @@
 // 앞뒤 공백 허용 X, 대소문자 구분 X
 const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-export function isValidEmail(email) {
+export function isValidEmail(email: string) {
   return emailRegex.test(email);
+}
+
+export async function fileToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file); // base64로 읽기
+
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
 }
