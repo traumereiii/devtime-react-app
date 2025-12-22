844379f1c0f580efecb58916762464420075604e
DongSeong Seo <sds901234@naver.com>
2025-12-22 23:07:48 +0900

    feat: UserProfile 조회 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import AppRoutes from "./AppRoutes.tsx";
 import DialogProvider from "@/providers/DialogProvider.tsx";
+import { useAuthStore } from "@/store/auth.ts";
+import { useUserProfileStore } from "@/store/user-profile.ts";
+import { fetchUserProfile } from "@/api/profile.ts";
+import { useEffect } from "react";
 
 function App() {
+  const authStore = useAuthStore();
+  const userProfileStore = useUserProfileStore();
+
+  useEffect(() => {
+    if (authStore.isAuthenticated()) {
+      fetchUserProfile().then((userProfile) => {
+        userProfileStore.actions.setUser(userProfile);
+      });
+    }
+  }, [authStore.accessToken]);
+
   return (
     <>
       <DialogProvider>
--- src/types.ts
+++ src/types.ts
@@ -14,3 +14,15 @@ export type UseMutationCallback<R> = {
   onMutate?: () => void;
   onSettled?: () => void;
 };
+
+export interface UserProfile {
+  email: string;
+  nickname: string;
+  profile?: {
+    career: string;
+    purpose: string;
+    goal: string;
+    techStacks: string[];
+    profileImage?: string;
+  };
+}
