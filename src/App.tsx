9e82e3a9cbaa17cf90528f7359d473cd3c1653ae
DongSeong Seo <sds901234@naver.com>
2025-12-22 21:41:47 +0900

    feat: axios interceptor 호출 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import AppRoutes from "./AppRoutes.tsx";
 import DialogProvider from "@/providers/DialogProvider.tsx";
-import AuthProvider from "@/providers/AuthProvider.tsx";
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <DialogProvider>
-          <AppRoutes />
-        </DialogProvider>
-      </AuthProvider>
+      <DialogProvider>
+        <AppRoutes />
+      </DialogProvider>
     </>
   );
 }
